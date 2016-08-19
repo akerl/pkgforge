f446bb3ec8b6745d0d2087f25d0d192ef162e81f
Les Aker <me@lesaker.org>
2016-08-19 01:44:39 -0400

    fix class method

--- lib/pkgforge.rb
+++ lib/pkgforge.rb
@@ -21,16 +21,16 @@ module PkgForge
     def new(*args)
       self::Forge.new(*args)
     end
-  end
 
-  ##
-  # Method for loading in DSL file
-  Contract Maybe[HashOf[Symbol => Any]] => self::Forge
-  def load_from_file(params = {})
-    file = params[:file] || DEFAULT_FILE
-    forge = Forge.new(params)
-    dsl = ForgeDSL.new(forge, params)
-    dsl.instance_eval(File.read(file), file)
-    forge
+    ##
+    # Method for loading in DSL file
+    Contract Maybe[HashOf[Symbol => Any]] => PkgForge::Forge
+    def load_from_file(params = {})
+      file = params[:file] || DEFAULT_FILE
+      forge = Forge.new(params)
+      dsl = ForgeDSL.new(forge, params)
+      dsl.instance_eval(File.read(file), file)
+      forge
+    end
   end
 end
