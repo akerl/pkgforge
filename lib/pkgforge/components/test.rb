1bdd4d44a602f05d779fdc50ff33d890cba3200b
Les Aker <me@lesaker.org>
2016-09-01 23:54:12 -0400

    fix variable scope

--- lib/pkgforge/components/test.rb
+++ lib/pkgforge/components/test.rb
@@ -31,9 +31,13 @@ module PkgForge
 
     private
 
+    Contract None => String
+    def lib_path_file
+      '/etc/ld-musl-x86_64.path'
+    end
+
     Contract Func[None => nil] => nil
     def lib_override
-      lib_path_file = '/etc/ld-musl-x86_64.path'
       old_lib_paths = File.read(lib_path_file) if File.exist?(lib_path_file)
       puts "Setting library path: #{ld_library_path}"
       File.open(lib_path_file, 'w') { |fh| fh << ld_library_path }
--- lib/pkgforge/version.rb
+++ lib/pkgforge/version.rb
@@ -1,5 +1,5 @@
 ##
 # Declare version number
 module PkgForge
-  VERSION = '0.4.12'.freeze
+  VERSION = '0.4.13'.freeze
 end
