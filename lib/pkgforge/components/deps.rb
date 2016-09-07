b7ea1b42f8a3fff824df44ee89a8e0a9bffd9f80
Les Aker <me@lesaker.org>
2016-09-07 01:51:54 -0400

    allow removal of linker archives

--- lib/pkgforge/components/deps.rb
+++ lib/pkgforge/components/deps.rb
@@ -4,21 +4,40 @@ module PkgForge
   ##
   # Add dep methods to Forge
   class Forge
-    attr_writer :deps
+    attr_writer :deps, :remove_linker_archives
 
     Contract None => HashOf[Symbol => String]
     def deps
       @deps ||= {}
     end
 
+    Contract None => Bool
+    def remove_linker_archives?
+      @remove_linker_archives ||= false
+    end
+
     private
 
     Contract None => nil
     def prepare_deps!
+      download_deps!
+      remove_linker_archives! if remove_linker_archives?
+    end
+
+    Contract None => nil
+    def download_deps
       deps.each do |dep_name, dep_version|
         url = "https://github.com/#{org}/#{dep_name}/releases/download/#{dep_version}/#{dep_name}.tar.gz" # rubocop:disable Metrics/LineLength
         open(tmpfile(dep_name), 'wb') { |fh| fh << open(url, 'rb').read }
         run_local "tar -x -C #{tmpdir(dep_name)} -f #{tmpfile(dep_name)}"
+      end
+      nil
+    end
+
+    Contract None => nil
+    def remove_linker_archives!
+      deps.keys.each do |dep_name|
+        File.unlink(*Dir.glob("#{tmpdir(dep_name)}/**/*.la"))
       end
       nil
     end
@@ -32,6 +51,11 @@ module PkgForge
       def deps(value)
         @forge.deps = value
         nil
+      end
+
+      Contract Bool => nil
+      def remove_linker_archives(value)
+        @forge.remove_linker_archives = value
       end
     end
   end
--- lib/pkgforge/version.rb
+++ lib/pkgforge/version.rb
@@ -1,5 +1,5 @@
 ##
 # Declare version number
 module PkgForge
-  VERSION = '0.6.1'.freeze
+  VERSION = '0.6.2'.freeze
 end
