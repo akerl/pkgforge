ee7782a57947c54213be8d37c07f240e74b25199
Les Aker <me@lesaker.org>
2017-01-05 10:14:39 -0800

    correctly swap method names

--- lib/pkgforge/components/state.rb
+++ lib/pkgforge/components/state.rb
@@ -5,14 +5,14 @@ module PkgForge
   # Add state methods to Forge
   class Forge
     Contract String => nil
-    def write_state!(statefile)
+    def load_state(statefile)
       state = JSON.parse(File.read(statefile))
       @tmpfiles = state[:tmpfiles]
       @tmpdirs = state[:tmpdirs]
     end
 
     Contract String => nil
-    def load_state(statefile)
+    def write_state!(statefile)
       File.open(statefile, 'w') do |fh|
         fh << {
           tmpfiles: @tmpfiles,
