1724b4cad71b65204e153e48958f7caf0249117e
Les Aker <me@lesaker.org>
2017-02-14 11:13:28 -0800

    fix typo

--- lib/pkgforge/components/deps.rb
+++ lib/pkgforge/components/deps.rb
@@ -6,7 +6,7 @@ module PkgForge
   class Forge
     attr_writer :deps, :remove_linker_archives, :remove_pkgconfig_files
 
-    Contract None => HashOf[Symbol => [String, Hash]]
+    Contract None => HashOf[Symbol => Or[String, Hash]]
     def deps
       @deps ||= {}
     end
